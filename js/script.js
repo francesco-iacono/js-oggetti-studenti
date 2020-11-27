f17050a523efea9362faa2c6728c3d10d0f589ed
francesco <f.iacono89@gmail.com>
2020-11-27 14:52:56 +0100

    primo commit

---
+++ js/script.js
@@ -0,0 +1,55 @@
+$(document).ready(
+  function () {
+    // Obiettivi dell'esercizio:
+    // 1
+    // Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età. Stampare a schermo attraverso il for in tutte le proprietà;
+
+    // Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età.
+    var studente = {
+      nome: "Francesco",
+      cognome: "Iacono",
+      eta: 31
+    };
+    console.log(studente);
+    // Stampare a schermo attraverso il for in tutte le proprietà;
+    for (var key in studente) {
+    console.log(studente[key]);
+    }
+    // ----------------------------------------------------------
+    // 2
+    // Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome;
+    var studenti = [
+      {
+        nome: "Francesco",
+        cognome: "Iacono",
+      },
+      {
+        nome: "Marco",
+        cognome: "Somma",
+      },
+      {
+        nome: "Anna",
+        cognome: "Di Maio",
+      }
+    ];
+    console.log(studenti);
+
+    for (var key in studenti) {
+    console.log((studenti[key].nome), (studenti[key].cognome));
+    document.getElementById('nomeCognome').innerHTML += (studenti[key].nome) + (studenti[key].cognome) + "<br>";
+    // Ciclare su tutti gli studenti e stampare per ognuno nome e cognome;
+    $('p').hide();
+    $('h1').click(
+        function functionName() {
+        $('p').show();
+        }
+      )
+    }
+
+    // Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+    var nome = prompt('Il tuo nome');
+    
+    // var cognome = promp('Il tuo cognome');
+    // var eta = prompt('La tua eta');
+  }
+);
