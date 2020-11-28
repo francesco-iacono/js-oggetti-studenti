394fb408c58b6dad2e57dab528c511dd870c2e98
francesco <f.iacono89@gmail.com>
2020-11-28 11:19:24 +0100

    last

--- js/script.js
+++ js/script.js
@@ -25,27 +25,16 @@ $(document).ready(
       },
       {
         nome: "Marco",
-        cognome: "Calvieri",
+        cognome: "Somma",
       },
       {
-        nome: "Riccardo",
-        cognome: "Ferrari",
+        nome: "Anna",
+        cognome: "De Maio",
       }
     ];
     console.log(studenti);
 
-    for (var key in studenti) {
-    console.log((studenti[key].nome), (studenti[key].cognome));
-    document.getElementById('nome').innerHTML += "<li>" + (studenti[key].nome) +  "</li>";
-    document.getElementById('cognome').innerHTML += "<li>" +  (studenti[key].cognome) + "</li>";
-    // Ciclare su tutti gli studenti e stampare per ognuno nome e cognome;
-    $('ol').hide();
-    $('h1').click(
-        function () {
-        $('ol').toggle();
-        }
-      )
-    }
+
     // 3
     // Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
     var nuovoStudente = {};
@@ -56,6 +45,19 @@ $(document).ready(
     var chiediEta = parseInt(prompt('La tua eta'));
     nuovoStudente.eta = chiediEta;
     studenti.push(nuovoStudente);
+
+    for (var key in studenti) {
+    console.log((studenti[key].nome), (studenti[key].cognome));
+    document.getElementById('nome').innerHTML += "<li>" + (studenti[key].nome) +  "</li>";
+    document.getElementById('cognome').innerHTML += "<li>" +  (studenti[key].cognome) + "</li>";
+    // Ciclare su tutti gli studenti e stampare per ognuno nome e cognome;
+    $('#nome, #cognome').hide();
+    $('h1').click(
+        function () {
+        $('#nome, #cognome').show(2000);
+        }
+      )
+    }
 
   }
 );
